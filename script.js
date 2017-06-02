96c72628be9ca1092544568a492c050a0b22fe41
Katarzyna Michalak <katarzynacmichalak@gmail.com>
2017-06-02 21:09:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var a=3;
+var h=7;
+
+
+
+function getTriangleArea(a, h) {
+if ((a < 0) || (h < 0)) 
+	console.log("incorrect data")
+else if ((a > 0) && (h > 0)) 
+	return total
+}
+var total = a*h/2
+
+
+console.log(getTriangleArea(10, 6))
+
+
+/*Patryk - tutaj nie mam pomyslu: Zrób trzy zmienne, które będą posiadały pole trójkąta, 
+za każdym razem przekazując różne liczby w parametrze. 
+Podpowiedź: var triangle1Area = getTriangleArea(10, 15);*/
+
+
+
