466aaa2fa21bb85bc82997f6375a70344eed57d1
Katarzyna Michalak <katarzynacmichalak@gmail.com>
2017-06-04 20:23:14 +0200

    poprawka

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 var a=3;
 var h=4;
-var triangle1Area = getTriangleArea(10,15); //Patryk i co te zmienne wtedy robią? :)
-var triangle2Area = getTriangleArea(15,20);
-var triangle3Area = getTriangleArea(20,25); 
+
 
 
 function getTriangleArea(a, h) {
 if ((a < 0) || (h < 0)) 
-	return ("incorrect data")
-else (a, h) //nie wiem, czy o to chodzilo :)
-	return total
+	return "incorrect data";
+else (a, h) /
+	return a*h/2;
 }
-var total = a*h/2
+var a*h/2;
+var triangle1Area = getTriangleArea(10,15); 
+var triangle2Area = getTriangleArea(15,20);
+var triangle3Area = getTriangleArea(20,25); 
 
 
-console.log(getTriangleArea(10, 6))
+console.log(getTriangleArea(10, 6));
 
 
 
