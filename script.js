ffaf40eac79e88ee7cef524439aeba55bea48e88
Katarzyna Michalak <katarzynacmichalak@gmail.com>
2017-06-05 20:59:03 +0200

    correction

--- script.js
+++ script.js
@@ -3,21 +3,22 @@ var h=4;
 
 
 
+
 function getTriangleArea(a, h) {
 if ((a < 0) || (h < 0)) 
 	return "incorrect data";
-else (a, h) /
-	return a*h/2; //Patryk, to niestety nie dziala :(
+else 
+	return a*h/2;
 }
-var a*h/2;
+
 var triangle1Area = getTriangleArea(10,15); 
 var triangle2Area = getTriangleArea(15,20);
 var triangle3Area = getTriangleArea(20,25); 
 
 
-console.log(getTriangleArea(10, 6));
+console.log(getTriangleArea(a,h));
 
 
 
 
-
+ 
